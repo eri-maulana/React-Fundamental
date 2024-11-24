a81374e8ac27deb3d66644606843c6fb9dc8e898
Eri Maulana <maulanabouble11@gmail.com>
2024-11-24 13:34:48 +0700

    1 - 3 (instalasi, pengenalan project dan components)

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Article from './components/Article'
+
+function App() {
+
+  return (
+    <>
+      <Article />
+      <Article />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Article.jsx
@@ -0,0 +1,5 @@
+function Article() {
+    return <div>ini adalah components pertama dan kedua</div>;
+}
+
+export default Article;
